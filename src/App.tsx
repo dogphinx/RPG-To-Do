e9274e3a36b5f364b40dbd6593e26af28db7708c
dogphinx <saydits08@gmail.com>
2021-07-28 16:24:05 +0900

    첫 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Character from './component/Character';
+import Inventory from './component/Inventory';
+import ToDoList from './component/ToDoList';
+import Status from './component/Status';
+import StatusHistory from './component/StatusHistory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Character">
+        <Character />
+        <Inventory />
+      </div>
+      <div className="ToDoList">
+        <ToDoList />
+      </div>
+      <div className="Status">
+      <Status />
+      <StatusHistory />
+      </div>
     </div>
   );
 }
